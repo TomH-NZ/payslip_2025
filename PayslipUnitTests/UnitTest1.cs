d20159a3b85341890e10c935ec3e1e9dc01fc120
Tom Hatcher <39353602+TomH-NZ@users.noreply.github.com>
2025-11-17 10:57:06 +1300

    Add TestCalc.cs, write basic unit test to confirm working as expected.

--- PayslipUnitTests/UnitTest1.cs
+++ PayslipUnitTests/UnitTest1.cs
@@ -3,7 +3,16 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GivenTwoNumbers_WhenAddingThemTogether_ThenTheCorrectResultReturned()
     {
+        //Arrange
+        var calc = new payslip_2025.TestCalc();
+        
+        //Act
+        var result = calc.TestCalculator(1, 2);
+        
+        //Assert
+        Assert.Equal(3, result);
     }
 }
+//Unit test written to verify that the test project is correctly set up and can reference the payslip_2025 project.
---
+++ payslip_2025/TestCalc.cs
@@ -0,0 +1,10 @@
+﻿namespace payslip_2025;
+
+public class TestCalc
+{
+    public int TestCalculator(int a, int b)
+    {
+        return a + b;
+    }
+}
+//Test class written to verify that the test project is correctly set up and can reference the payslip_2025 project.
